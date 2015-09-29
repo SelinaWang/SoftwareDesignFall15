4ebce8272c10eb53dd9966a431074d807eb2c105
Ziyu Wang <ziyu.wang@students.olin.edu>
2015-09-28 23:48:01 -0400

    Completed MP1

--- MP1/text_analysis.py
+++ MP1/text_analysis.py
@@ -1,50 +1,40 @@
-from pattern.en import *
+'''
+This script uses Pattern's sentiment analysis to find the average polarity and subjectivity of collected tweets about presidential candidates
+by Ziyu (Selina) Wang
+last modified: September 28, 2015
+'''
+from pattern.en import *	# Importing Patern to be utilized later
+# Creating a list with all the candidates names
 candidates = ['HillaryClinton','DonaldTrump','BernieSanders','BenCarson','JebBush','TedCruz','MarcoRubio','MikeHuckabee','RandPaul','CarlyFiorina','ScottWalker','JohnKasich',"MartinO'Malley",'ChrisChristie','JimWebb','RickSantorum','BobbyJindal','LincolnChafee','LindseyGraham','GeorgePataki','JimGilmore','JillStein']
+# Traverse through the list
 for candidate in candidates:
-	analysisName = candidate+'_analysis.txt'  # Name of text file coerced with +.txt
-	polarityName = candidate+'_polarity.txt'
-	subjectivityName = candidate+'_subjectivity.txt'
-	try:
-		with open(candidate+'_analysis.txt', 'a') as f1, open (candidate+'_polarity.txt','a') as f2, open (candidate+'_subjectivity.txt','a') as f3: # Trying to create a new file or open one
-			pass
-	except:
-		print('Something went wrong! Can\'t tell what?')
-	f1.close()
-	f2.close()
-	f3.close()
-
-for candidate in candidates:
+	# Creating three lists for storing data from the sentiment analysis
 	analysis = []
 	polarity = []
 	subjectivity = []
 	try:
-		with open(candidate+'.txt', 'r') as f1, open (candidate+'_analysis.txt','w') as f2, open (candidate+'_polarity.txt','w') as f3, open (candidate+'_subjectivity.txt','w') as f4:
+		with open(candidate+'.txt', 'r') as f1: # Trying to open the .txt file with all the tweets in it
+			# Traverse through the file line by line
 			for line in f1:
-				data = sentiment(line)
-				datastr = str(sentiment(line))
+				data = sentiment(line)	# run sentiment analysis on each tweet
+				# Storing the analysis data in the corresponding list
 				analysis.append(data)
-				f2.write(datastr+'\n')
-				datastr.split()
 				polarity.append(data[0])
-				f3.write(datastr[0]+'\n')
 				subjectivity.append(data[1])
-				f4.write(datastr[1]+'\n')
-	except IOError as e:
-		print 'Operation failed: %s' % e.strerror
-	f1.close();
-	f2.close();
-	f3.close();
-	f4.close();
-	averagePolarity = sum(polarity)/len(polarity)
-	print candidate + ' average polarity: ' + str(averagePolarity)
-	averageSubjectivity = sum(subjectivity)/len(subjectivity)
-	print candidate + ' average subjectivity: ' + str(averageSubjectivity)
-	# for data in analysis:
-	# 	print data.split()
-		#print data[3]
+	except:
+		print 'Running analysis failed'	# Throw an error if the analysis failed to execute
+	if analysis:	# if the analysis was succesful
+		# Calculating and displaying the number tweets collected
+		numberOfTweets = len(analysis)
+		print "Number of tweets about " + candidate + ': ' + str(numberOfTweets)
+		# Calculating and displaying the average polarity
+		averagePolarity = sum(polarity)/len(polarity)
+		print candidate + "'s average polarity: " + str(averagePolarity)
+		# Calculating and displaying the average subjectivity
+		averageSubjectivity = sum(subjectivity)/len(subjectivity)
+		print candidate + "'s average subjectivity: " + str(averageSubjectivity)
+	else:	# If there are no tweets about a candidate, display this information
+		print 'There is no tweets about ' + candidate + ' collected'
+	f1.close();	# Close the .txt file to clean up
 
 
-# for candidate in candidates:
-# 	file = open(candidate+'_analysis.txt', 'r')
-# 	for line in file:
-# 		int(line)
--- MP1/text_mining.py
+++ MP1/text_mining.py
@@ -1,19 +1,33 @@
-from pattern.web import *
-candidates = ['HillaryClinton','DonaldTrump','BernieSanders','BenCarson','JebBush','TedCruz','MarcoRubio','MikeHuckabee','RandPaul','CarlyFiorina','ScottWalker','JohnKasich',"MartinO'Malley",'ChrisChristie','JimWebb','RickSantorum','BobbyJindal','LincolnChafee','LindseyGraham','GeorgePataki','JimGilmore','JillStein']
+'''
+This script uses Pattern and Twitter API to collect tweets about presidential candidates in the past 30 seconds
+by Ziyu (Selina) Wang
+last modified: September 28, 2015
+'''
+from pattern.web import *	# Importing Patern to be utilized later
+# Creating a list with all the candidates names
+#candidates = ['HillaryClinton','DonaldTrump','BernieSanders','BenCarson','JebBush','TedCruz','MarcoRubio','RandPaul','CarlyFiorina','JohnKasich',"MartinO'Malley",'ChrisChristie','JimWebb','JillStein','RickSantorum','BobbyJindal','LincolnChafee','LindseyGraham','GeorgePataki','JimGilmore','MikeHuckabee','ScottWalker']
+candidates = ['GeorgePataki','JimWebb',"MartinO'Malley",'ChrisChristie','JimGilmore','LindseyGraham','LincolnChafee','BobbyJindal','RickSantorum','JillStein']
+# Creating a list with all the candidates names in reversed order (in case we need to traverse the original list backwards to collect data for all the candidates)
 reversedCandidates = list(reversed(candidates))
-#for candidate in candidates:
-for candidate in reversedCandidates:
-	fileName = candidate+'.txt'  # Name of text file coerced with +.txt
+# Traverse through the list
+for candidate in candidates:
+# for candidate in reversedCandidates:	# swap with the last line when traversing the original list backwards
+	fileName = candidate+'.txt'	# Name the text file under the candidate's name coerced with +.txt
 	try:
-		file = open(fileName,'a')   # Trying to create a new file or open one
+		file = open(fileName,'a')   # Trying to create a new file or open an existing one
 	except:
-		print('Something went wrong! Can\'t tell what?')
-	s = Twitter().stream(candidate)
+		print('Cannot create/open the file')	# If failed to create/open one, through an exception
+	s = Twitter().stream(candidate)	# using Pattern and Twitter API to collect tweets about the candidate
 	print candidate
-	for i in range(30):
+	# Storing the tweets in the past 30 seconds into the plain text file
+	for i in range(30):	# If there are tweets in the stream
 		time.sleep(1)
 		s.update(bytes=1024)
-		file.write(s[-1].text+'\n') if s else ''
-		print s[-1].text if s else ''
-	file.close()
+		if s:	
+			try:
+				file.write(s[-1].text+'\n')
+				print s[-1].text
+			except:
+				pass	# The tweet did not have text in it so skip to the next one
+	file.close()	# Close the .txt file to clean up
 
